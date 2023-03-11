ef27187d867f7b26f466c4e7005ea57587d0dd21
NicoFOESSEL <nicofoessel91740@gmail.com>
2023-03-11 11:39:57 +0100

    ajouts import
    En commentaire pour l'instant

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-main():
+import os
+import sys
+
+"""
+def importDnsScan():
+    try:
+        import dnscan
+    except:
+        os.system("sudo git clone \"https://github.com/rbsec/dnscan.git\"")
+        importDnsScan()
+def importShodan():
+    try:
+        import shodan
+    except:
+        os.system("sudo pip install shodan")
+        importShodan()
+def importTheHarvester():
+    try:
+        import theharvester
+    except:
+        os.system("sudo apt-get install thearvester")
+        importTheHarvester()
+def importUrlScan():
+    try:
+        import urlscan
+    except:
+        os.system("sudo apt-get install urlscan")  # uniquement noyaux linux
+        importUrlScan()
+
+def verifImport():
+    importDnsScan()
+    importShodan()
+    importTheHarvester()
+    importUrlScan()
+    print("imports ok")
+"""
+
+
+"""
+    /!\demander clé API shodan/!\ 
+"""
+#verifImport()
+
